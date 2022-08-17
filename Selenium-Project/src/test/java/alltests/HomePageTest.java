b335078ce623fe946b71341cf2be15d0a162776e
snhoque <shoque23@gmail.com>
2022-08-17 19:48:10 -0400

    Added another line

--- Selenium-Project/src/test/java/alltests/HomePageTest.java
+++ Selenium-Project/src/test/java/alltests/HomePageTest.java
@@ -21,5 +21,6 @@ public class HomePageTest {
     public void test1(){
         System.out.println("this is a test");
         System.out.println("Testing git");
+        System.out.println("Making another test");
     }
 }
